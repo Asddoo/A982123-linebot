907d8157447b765260b277a6bc176a7659bad032
qaz1238812 <qaz1238812@gmail.com>
2023-06-05 11:54:07 +0800

    35:07

--- A982123-linebot/LineBotApp.cs
+++ A982123-linebot/LineBotApp.cs
@@ -6,6 +6,9 @@ namespace A982123_linebot;
 public class LineBotApp : WebhookApplication
 {
     private readonly LineMessagingClient _messagingClient;
+
+    private static Dictionary<string, string> _pool = new Dictionary<string, string>();
+
     public LineBotApp(LineMessagingClient lineMessagingClient)
     {
         _messagingClient = lineMessagingClient;
@@ -24,17 +27,78 @@ public class LineBotApp : WebhookApplication
                 var channelId = ev.Source.Id;
                 //使用者Id
                 var userId = ev.Source.UserId;
-                    
+                //使用者輸入的文字
+                var text = ((TextEventMessage)ev.Message).Text;
+
+                if (PoolHasMsg(text))
+                {
+                    // 從記憶體池查詢資料
+                    string response = GetResponse(text);
+                    result = new List<ISendMessage>
+                    {
+                        new TextMessage(response)
+                    };
+                }
+                else
+                {
+                    if (CheckFormat(text))
+                    {
+                        //將資料寫入記憶體池
+                        TeachDog(text);
+                    }
+                }
+                /*
                 //回傳 hellow
                 result = new List<ISendMessage>
                 {
                     new TextMessage("小雞雞")
                 };
+                */
             }
                 break;
         }
 
         if (result != null)
             await _messagingClient.ReplyMessageAsync(ev.ReplyToken, result);
+    }
+
+    private bool PoolHasMsg(string InputMsg)
+    {
+        return _pool.ContainsKey(InputMsg);
+    }
+
+    private string GetResponse(string inputMsg)
+    {
+        return _pool[inputMsg];
+    }
+    
+    private bool CheckFormat(string inputMsg)
+    {
+        bool result = false;
+
+        try
+        {
+            string[] subs = inputMsg.Split(';');
+
+            if (subs.Length == 3)
+            {
+                if (subs[0] == "ABC")
+                {
+                    result = true;
+                }
+            }
+        }
+        catch (Exception exp)
+        {
+            Console.WriteLine(exp);
+            throw;
+        }
+        
+        return result;
+    }
+
+    private void TeachDog(string inputMsg)
+    {
+        
     }
 }
